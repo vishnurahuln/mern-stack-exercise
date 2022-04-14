bc46ee9ab7ea321d9e64a0614ab79ef60b6269ca
Vishnu Rahul <vishnurahuln@gmail.com>
2022-04-14 00:00:01 -0500

    final fixes and added gh-pages dependency

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/users/')
+    axios.get('http://localhost:5001/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -74,10 +74,10 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('http://localhost:5001/exercises/add', exercise)
       .then(res => console.log(res.data));
 
-    window.location = '/';
+    //window.location = '/';
   }
 
   render() {
